ce6f4cc857fc74ef120004418d3edf16f1d8f0b0
Amit Kumar <mishraamit300266@gmail.com>
2025-07-15 16:58:27 +0530

    Form using Flask

--- my_first_flask_app/app.py
+++ my_first_flask_app/app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def form():
+    return render_template('form.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/greet', methods=['POST'])
+def greet():
+    user = request.form['user_name']
+    return render_template('greet.html', name=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
